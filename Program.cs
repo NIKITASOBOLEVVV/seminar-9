90a1caee2cb5858c4e384ca15761678194625bae
NIKITA <putnik824@gmail.com>
2022-10-27 15:53:44 +0700

    9 seminar home1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в 
+//промежутке от N до 1. Выполнить с помощью рекурсии.
+/*
+void UU(int n)
+{
+    if (n > 0)
+    {
+        Console.Write("{0} ", n);
+        UU(n - 1);
+    }
+}
+Console.WriteLine("enter number");
+int i = Convert.ToInt32(Console.ReadLine());
+UU(i);
+
+
+//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+ void Sum(int m, int n, int sum)
+ {
+     sum = sum + n;
+     if (n <= m)
+    {
+         Console.Write($" {sum} ");
+         return;
+     }
+     Sum(m, n - 1, sum);
+
+ }
+ Console.WriteLine("enter number");
+ int m = Convert.ToInt32(Console.ReadLine());
+ int n = Convert.ToInt32(Console.ReadLine());
+ int temp = m;
+
+ if (m > n)
+ {
+     m = n;
+     n = temp;
+ }
+ Console.Write("sum =");
+ Sum(m, n, temp = 0);*/
+
